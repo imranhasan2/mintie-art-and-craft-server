909ecfe686f6b4e075d84d898d258f1221b30b33
imranhasan2 <imranhasan030303@gmail.com>
2024-04-28 17:51:31 +0600

    created user database in mongodb

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ app.use(express.json())
 
 
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhkveen.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 console.log(uri)
@@ -32,12 +27,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const userCollection = client.db('userDB').collection('user')
+
+        app.post('/user', async (req, res) => {
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
