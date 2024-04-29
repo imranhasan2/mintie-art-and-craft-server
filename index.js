1b86a79788ce7aca3e6e63fbb1671aea43764acf
imranhasan2 <imranhasan030303@gmail.com>
2024-04-29 18:26:12 +0600

    get data from data base and send to client

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -31,7 +31,7 @@ async function run() {
 
         const userCollection = client.db('userDB').collection('user')
 
-        const craftCollection =client.db('craftDB').collection('craft')
+        const craftCollection = client.db('craftDB').collection('craft')
 
         app.post('/user', async (req, res) => {
             const user = req.body
@@ -39,11 +39,20 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/craft',async(req,res) => {
+            const cursor = craftCollection.find()
+            const result = await cursor.toArray()
+            res.send (result)
+
+        })
+
+        app.post('/craft', async (req, res) => {
+            const craft = req.body
+            console.log(craft)
+            const result = await craftCollection.insertOne(craft)
+            res.send(result)
 
 
-        app.post('/craft',async(req,res) => {
-            const item = req.body
-            console.log(item)
         })
 
 
